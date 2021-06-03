009e642729c7008ed85f0dac562446d253accc8f
youcef101 <youcef.khadem12@gmail.com>
2021-06-03 18:36:15 +0100

    Building SubHeader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import SubHeader from './components/SubHeader';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SubHeader />
 
     </div>
   );
--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -1,11 +1,68 @@
 import React from 'react'
+import styled from 'styled-components'
+import MenuIcon from '@material-ui/icons/Menu';
 
 function SubHeader() {
     return (
-        <div>
+        <Container>
+            <MenuContainer>
+                <Menu>
+                    <MenuIc />
+                </Menu>
+                <MenuText>
+                    All
+                </MenuText>
+            </MenuContainer>
+            <MenuNavContainer>
+                <a>Today's Deals</a>
+                <a>Buy Again</a>
+                <a>Customer Service</a>
+                <a>Registry</a>
+                <a>Sell</a>
+                <a>Gift Cards</a>
+            </MenuNavContainer>
+            <CovidContainer>
+                <Covid>Amazon's response to COVID-19</Covid>
+            </CovidContainer>
 
-        </div>
+        </Container>
     )
 }
 
 export default SubHeader
+const Container = styled.div`
+height:40px;
+background:#000d1a;
+display:flex;
+align-items:center;
+
+`
+const MenuContainer = styled.div`
+display:flex;
+align-items:center;
+padding:20px;
+cursor:pointer;
+
+`
+const Menu = styled.div`
+display:flex;
+align-items:center;
+
+`
+const MenuIc = styled(MenuIcon)``
+const MenuText = styled.span`
+font-weight:500;
+`
+const MenuNavContainer = styled.div`
+flex:1;
+a{
+    padding:0 10px;
+    cursor:pointer;
+}
+`
+const CovidContainer = styled.div`
+padding:20px;
+cursor:pointer;
+`
+const Covid = styled.span`
+`
